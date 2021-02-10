41f359decc1c60cf46dcc6557bc64db47995740e
SamArsanios <samson.kibrom@hotmail.com>
2021-02-10 11:14:33 +0300

    Add validation error messages to forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   
-  validates :username, presence: true
+  validates :username, presence: true, length: { minimum: 5 }
   validates :password, presence: true
   validates :email, presence: true
 end
