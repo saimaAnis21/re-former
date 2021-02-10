676a5cae40733a71b0a4488ff26388defd8b5c3b
SamArsanios <samson.kibrom@hotmail.com>
2021-02-10 10:35:08 +0300

    Add the edit method to update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,26 @@ class UsersController < ApplicationController
     end
 
     def create
-    #   @user = User.new(user_params)
-    @user = User.new(username: params[:username], password:params[:password], email:params[:email])
+      @user = User.new(user_params)
+      # @user = User.new(username: params[:username], password:params[:password], email:params[:email])
       if @user.save
         redirect_to new_user_path
       else
         render :new
+      end
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+
+      if @user.update(user_params)
+        redirect_to edit_user_path
+      else
+        render :edit
       end
     end
 
