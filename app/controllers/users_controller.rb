aae7db012705e47bf47ef0ed19a25db26d3c7a40
SamArsanios <samson.kibrom@hotmail.com>
2021-02-10 11:17:35 +0300

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,35 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
 
-    def new
-      @user = User.new
-    end
-
-    def create
-      @user = User.new(user_params)
-      # @user = User.new(username: params[:username], password:params[:password], email:params[:email])
-      if @user.save
-        redirect_to new_user_path
-      else
-        render :new
-      end
-    end
-
-    def edit
-      @user = User.find(params[:id])
-    end
-
-    def update
-      @user = User.find(params[:id])
-
-      if @user.update(user_params)
-        redirect_to edit_user_path
-      else
-        render :edit
-      end
-    end
-
-    private
-    def user_params
-      params.require(:user).permit(:username,:password,:email)
+  def create
+    @user = User.new(user_params)
+    # @user = User.new(username: params[:username], password:params[:password], email:params[:email])
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  
   validates :username, presence: true, length: { minimum: 5 }
   validates :password, presence: true
   validates :email, presence: true
